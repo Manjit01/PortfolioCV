18bc79261a0b43401e7e2a6cb5c21c46787f297d
maahibajwa36 <maahibajwa36@outlook.com>
2020-09-09 05:58:22 +1200

    Deploy to special repository

---
+++ precache-manifest.4484e1e5e67cf54b7321fb483ed61165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef7e163de9848389ce1088675cf6ed4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcfd6c7156d09c9cc96e",
+    "url": "/static/css/main.5c2ae854.chunk.css"
+  },
+  {
+    "revision": "b99a1a4510645dea9a48",
+    "url": "/static/js/2.06808d83.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.06808d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcfd6c7156d09c9cc96e",
+    "url": "/static/js/main.630228c9.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  }
+]);
