0f3d26a2152c1a7c7990f99ff4455bd27a24b069
maahibajwa36 <maahibajwa36@outlook.com>
2020-09-08 18:34:41 +1200

    Updates

---
+++ precache-manifest.d4f22e34fe8f604d4a8ce06463c918f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1965ce32a79698cad79747603c829a88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95c2c2abd109f6d48763",
+    "url": "/static/css/main.5c2ae854.chunk.css"
+  },
+  {
+    "revision": "b99a1a4510645dea9a48",
+    "url": "/static/js/2.06808d83.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.06808d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c2c2abd109f6d48763",
+    "url": "/static/js/main.f599ab61.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  }
+]);
